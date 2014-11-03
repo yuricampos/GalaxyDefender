2ed1e46fa072e3f0a110f42c1267de1ee5efaf5a
Yuri Campos Ferreira da Silva <yuricampos@Yuris-MacBook-Pro.local>
2014-11-03 13:17:06 -0500

    creating transitions

--- GalaxyDefender/Assets/Scripts/TransitionThreeScript.cs
+++ GalaxyDefender/Assets/Scripts/TransitionThreeScript.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class TransitionThreeScript : MonoBehaviour {
+	void OnGUI()
+	{
+		const int buttonWidth = 120;
+		const int buttonHeight = 60;
 
-	// Use this for initialization
-	void Start () {
-	
+		
+		if (
+			GUI.Button(
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			),
+			"Back to menu"
+			)
+			)
+		{
+			Application.LoadLevel("Menu");
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
--- GalaxyDefender/Assets/Scripts/TransitionTwoScript.cs
+++ GalaxyDefender/Assets/Scripts/TransitionTwoScript.cs
@@ -2,14 +2,38 @@
 using System.Collections;
 
 public class TransitionTwoScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+	void OnGUI()
+	{
+		const int buttonWidth = 120;
+		const int buttonHeight = 60;
+		
+		if (
+			GUI.Button(
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(1 * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			),
+			"LEVEL 3!"
+			)
+			)
+		{
+			Application.LoadLevel("LevelThree");
+		}
+		
+		if (
+			GUI.Button(
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			),
+			"Back to menu"
+			)
+			)
+		{
+			Application.LoadLevel("Menu");
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
